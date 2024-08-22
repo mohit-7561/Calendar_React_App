1498eab4ea41c18da21075853151962ac51d6a2e
mohit-7561 <mohitsinghania666r@gmail.com>
2024-08-22 14:50:36 +0530

    implemented month in the home page

--- calendar-react-app/src/pages/CalendarPage.jsx
+++ calendar-react-app/src/pages/CalendarPage.jsx
@@ -1,13 +1,38 @@
-import Calendar from "../components/CalendarView";
+import React from "react";
+import CalendarView from "../components/CalendarView";
 import EventForm from "../components/EventForm";
+import styles from "./CalendarPage.module.css"; // Import the CSS module
 
-const CalendarView = () => {
+const CalendarPage = () => {
+  // Get the current month and year
+  const today = new Date();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const currentMonth = monthNames[today.getMonth()];
+  const currentYear = today.getFullYear();
+
   return (
     <div>
-      <Calendar />
+      {/* Apply the CSS class to the h1 */}
+      <h1 className={styles.calendarHeader}>
+        Calendar - {currentMonth} {currentYear}
+      </h1>
       <EventForm />
+      <CalendarView />
     </div>
   );
 };
 
-export default CalendarView;
+export default CalendarPage;
